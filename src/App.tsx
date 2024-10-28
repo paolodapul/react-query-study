00396f48ea21001fd404720ceeac18195d12d16f
Paolo Dapul <paolodapultech@gmail.com>
2024-10-28 13:37:11 +0800

    implement useQuery to fetch data using API client

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import { api } from "./lib/api-client";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Post {
+  id: string;
+  title: string;
+  body: string;
 }
 
-export default App
+// eslint-disable-next-line react-refresh/only-export-components
+export const getPosts = async (): Promise<Post[]> => {
+  const posts = await api.get<Post[]>("/posts");
+  return posts.data;
+};
+
+function App() {
+  const { isPending, isError, data, error } = useQuery<Post[]>({
+    queryKey: ["todos"],
+    queryFn: getPosts,
+  });
+
+  if (isPending) {
+    return <span>Loading ...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <ul>
+      {data.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export { App };
